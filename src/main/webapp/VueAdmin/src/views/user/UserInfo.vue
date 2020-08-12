79105b221adf2730c5bf435354b948665ac020d5
macro shi <122260569@qq.com>
2020-08-12 15:25:20 +0800

    111

---
+++ src/main/webapp/VueAdmin/src/views/user/UserInfo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+<style scoped>
+
+.aaaa{
+  padding-top: 11px;
+  text-align: center;
+
+  margin: auto;
+}
+</style>
